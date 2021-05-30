6a967a94e441983bf13d6ccfb490a5a71832a334
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2021-05-30 21:16:33 +0200

    inital commit

---
+++ app.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const key = "c1a299b1b07f504df816ef7dcf5ad793";
+
+let movies = [];
+const moviesEl = document.getElementById("movies");
+const searchEl = document.getElementById("search");
+const searchBtn = document.getElementById("search-btn");
+
+searchBtn.addEventListener("click", () => {
+  movies = [];
+  moviesEl.innerHTML = "";
+  searchMovie(searchEl.value);
+  searchEl.value = "";
+});
+
+searchEl.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    movies = [];
+    moviesEl.innerHTML = "";
+    searchMovie(searchEl.value);
+    searchEl.value = "";
+  }
+});
+
+async function searchMovie(query) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${query}`;
+  const imgUrl = `https://api.themoviedb.org/3/configuration?api_key=${key}`;
+  const result = await fetch(url);
+  const data = await result.json();
+  console.log(data);
+
+  const imgData = await fetch(imgUrl);
+  const imgResult = await imgData.json();
+
+  const baseImgUrl = imgResult.images.base_url;
+
+  let postSize = imgResult.images.poster_sizes[4];
+
+  data.results.forEach((movie) => {
+    const completeImgUrl = `${baseImgUrl}/${postSize}${movie.poster_path}`;
+    movies.push({
+      title: movie.original_title,
+      poster: completeImgUrl,
+    });
+  });
+
+  renderMovies();
+}
+
+function renderMovies() {
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    const h3Title = document.createElement("h3");
+    h3Title.classList.add("title");
+    h3Title.textContent = movie.title;
+
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("img-container");
+
+    const img = document.createElement("img");
+    img.src = movie.poster;
+
+    imgContainer.appendChild(img);
+
+    movieEl.appendChild(h3Title);
+    movieEl.appendChild(imgContainer);
+
+    moviesEl.appendChild(movieEl);
+  });
+}
